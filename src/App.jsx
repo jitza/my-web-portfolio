fdd27da267ef028efd5bf905557e102d4ed6c5fa
jitza <jitza@shc.edu.bz>
2022-11-19 14:23:11 -0600

    Removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import { FaBeer } from 'react-icons/fa';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="text-3xl font-bold underline">Hello<FaBeer /></div>
+    <div className="text-3xl flex items-center justify-center font-bold underline">Hello<FaBeer /></div>
   )
 }
 
