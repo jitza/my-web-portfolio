81fb2c6ea1cad120ab2d6c18902c40ae0cc38648
jitza <jitza@shc.edu.bz>
2022-12-01 16:06:18 -0600

    Implemented navegation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "About",
+    },
+    {
+      id: 3,
+      link: "Services",
+    },
+    {
+      id: 6,
+      link: "Portfolio",
+    },
+    {
+      id: 7,
+      link: "Experience",
+    },
+    {
+      id: 8,
+      link: "Contact",
+    },
+  ];
+  return (
+    <div className="bg-purple text-pink absolute inset-x-0 bottom-0 p-8">
+      <ul className="flex justify-center">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="cursor-pointer uppercase px-4 hover:scale-110 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
