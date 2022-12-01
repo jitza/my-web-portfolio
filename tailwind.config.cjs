69b5d294a52f9163a652417b08bd3ad165eb998e
jitza <jitza@shc.edu.bz>
2022-12-01 16:04:54 -0600

    Added theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      pink: "#f7c1bb",
+      purple: "#1f0322",
+    },
     extend: {},
   },
   plugins: [],
