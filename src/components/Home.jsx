fe77ad7c501ca4508043867271b23d88f24344e3
jitza <jitza@shc.edu.bz>
2022-12-01 16:06:47 -0600

    Created home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import MyProfile from "../assets/MyProfile.jpeg";
 
-export const Home = () => {
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div className="bg-pink h-screen w-full flex justify-center items-center">
+      <div>
+        <img src={MyProfile} alt="My Profile" className="h-96" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
+export default Home;
