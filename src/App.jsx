ab4407967d57e0a1c5c7a7e84f0b4bffc6866a7d
jitza <jitza@shc.edu.bz>
2022-12-02 21:54:37 -0600

    Added routes with path and element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { FaBeer } from "react-icons/fa";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 };
