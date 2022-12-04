9efa02a6ee4fe2c919d811877592c610088f041d
jitza <jitza@shc.edu.bz>
2022-12-03 21:30:46 -0600

    Implemented home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,42 @@
 import React from "react";
-import MyProfile from "../assets/MyProfile.jpeg";
-import { BsFacebook, BsInstagram, BsGithub } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import ReactCurvedText from "react-curved-text";
+import StackProfile from "./StackProfile";
+import SocialLinks from "./SocialLinks";
 
 const Home = () => {
   return (
-    <div className="bg-pink h-screen w-full ">
-      <div className="text-purple font-bold text-xl">
-        Jenny Emerita Itza Front End Developer
+    <div className="bg-pink h-screen w-full flex justify-center items-center">
+      <div className="absolute top-4 right-4">
+        <ReactCurvedText
+          width={180}
+          height={170}
+          cx={85}
+          cy={87}
+          rx={65}
+          ry={61}
+          startOffset={0}
+          reversed={true}
+          text="Jenny Emerita Itza Front End Developer"
+          textProps={{
+            style: { fontSize: 24, fontFamily: "serif" },
+          }}
+          textPathProps={{ style: { fill: "#1f0322" } }}
+          tspanProps={null}
+          ellipseProps={null}
+          svgProps={360}
+        />
       </div>
-      <div className="flex justify-center">
-        <div className="columns-2">
-          <div className="bg-purple boder-8">
-            <img src={MyProfile} alt="My Profile" className="h-96 p-4" />
-          </div>
-
-          <div className="grid grid-rows-5">
-            <h1 className="text-7xl text-purple font-serif">Jenny Itza</h1>
-            <div className="bg-purple h-1 w-72" />
-            <div className="bg-purple h-1 w-72" />
-            <h2 className="text-xl font-bold text-purple uppercase">
-              Front End Developer
-            </h2>
-            <div className="flex flex-row">
-              <a>
-                <BsGithub />
-              </a>
-              <a>
-                <BsFacebook />
-              </a>
-              <a>
-                <BsInstagram />
-              </a>
-              <a>
-                <BsGithub />
-              </a>
-              <a>
-                <MdEmail />
-              </a>
-            </div>
-          </div>
+      <div className="grid grid-cols-2 grid-flow-row">
+        <div>
+          <StackProfile />
+        </div>
+        <div className="grid grid-row-3 gap-3">
+          <h1 className="text-7xl text-purple font-serif">Jenny Itza</h1>
+          <div className="bg-purple h-2 w-72" />
+          <h2 className="text-xl font-bold text-purple uppercase">
+            Front End Developer
+          </h2>
+          <SocialLinks />
         </div>
       </div>
     </div>
