508776f1b9af1f2cd58ab33d7a13ce214520a8fa
jitza <jitza@shc.edu.bz>
2022-12-15 13:31:53 -0600

    Implemented mobile home display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactCurvedText from "react-curved-text";
 import StackProfile from "./StackProfile";
 import SocialLinks from "./SocialLinks";
+import myProfile from "../assets/myProfile.jpeg";
 
 const Home = () => {
   return (
     <div className="bg-pink h-screen w-full flex justify-center items-center">
-      <div className="absolute top-4 right-4">
+      <div className="invisible md:visible absolute top-4 right-4">
         <ReactCurvedText
           width={180}
           height={170}
@@ -25,13 +26,23 @@ const Home = () => {
           ellipseProps={null}
           svgProps={360}
         />
+        <h1 className="invisible md:visible text-5xl text-purple absolute top-16 right-20">
+          JI
+        </h1>
       </div>
-      <div className="grid grid-cols-2 grid-flow-row">
-        <div>
+      <div className="flex flex-col gap-4 md:grid grid-cols-2 grid-flow-row">
+        <img
+          src={myProfile}
+          alt="myProfile"
+          className="h-96 shadow-purple shadow-2xl md:hidden"
+        />
+        <div className="invisible md:visible">
           <StackProfile />
         </div>
         <div className="grid grid-row-3 gap-3">
-          <h1 className="text-7xl text-purple font-serif">Jenny Itza</h1>
+          <h1 className="text-5xl text-purple font-serif md:text-7xl">
+            Jenny Itza
+          </h1>
           <div className="bg-purple h-2 w-72" />
           <h2 className="text-lg font-bold text-purple uppercase">
             Front End Developer
