c63a7cbacd9587cd6355994e463e780b95c8f8ec
jitza <jitza@shc.edu.bz>
2022-12-29 10:39:24 -0600

    Added primary and secondary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      pink: "#f7c1bb",
-      purple: "#1f0322",
+    extend: {
+      colors: {
+        //   brown: {
+        //     50: "#fdf8f6",
+        //     100: "#f2e8e5",
+        //     200: "#eaddd7",
+        //     300: "#e0cec7",
+        //     400: "#d2bab0",
+        //     500: "#bfa094",
+        //     600: "#a18072",
+        //     700: "#977669",
+        //     800: "#846358",
+        //     900: "#43302b",
+        //   },
+        primary: "#7b0d1e",
+        secondary: "#f7c1bb",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
