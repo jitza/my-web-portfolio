05dc2d9d55573d4f03b8c1acbe9256d9e3036e51
jitza <jitza@shc.edu.bz>
2023-01-01 21:32:17 -0600

    Removed react curved words

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactCurvedText from "react-curved-text";
-import StackProfile from "./StackProfile";
 import SocialLinks from "./SocialLinks";
 import myProfile from "../assets/myProfile.jpeg";
 import myProfileimg from "../assets/myProfile.png";
@@ -10,29 +8,6 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="bg-primary h-screen w-full flex justify-center items-center">
-      {/* <div className="hidden md:absolute top-4 right-4">
-        <ReactCurvedText
-          width={180}
-          height={170}
-          cx={85}
-          cy={87}
-          rx={66}
-          ry={61}
-          startOffset={0}
-          reversed={true}
-          text="Jenny Emerita Itza Interactive Developer"
-          textProps={{
-            style: { fontSize: 24, fontFamily: "serif" },
-          }}
-          textPathProps={{ style: { fill: "#7b0d1e" } }}
-          tspanProps={null}
-          ellipseProps={null}
-          svgProps={null}
-        />
-        <h1 className="hidden md:text-5xl text-secondary absolute top-16 right-20">
-          JI
-        </h1>
-      </div> */}
       <div className="flex flex-col gap-8 md:grid grid-cols-2 grid-flow-row">
         <div className="flex justify-center mt-20 md:hidden">
           <img
@@ -60,7 +35,7 @@ const Home = () => {
           <SocialLinks />
           <div className="hidden md:flex">
             <Link
-              to="about"
+              to="portfolio"
               className="group text-white w-fit px-6 py-2 flex items-center rounded-md bg-rose-500 cursor-pointer hover:bg-rose-700"
             >
               Portfolio
