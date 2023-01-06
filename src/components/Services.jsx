be8b686dca650decf4a6d0412b71dd37acd567db
jitza <jitza@shc.edu.bz>
2023-01-05 21:14:04 -0600

    Replaced app dev with game dev

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import graphicDesign from "../assets/graphicDesign.png";
-import appDevelopment from "../assets/appDevelopment.png";
+import gameDevelopment from "../assets/gameDevelopment.png";
 import webDevelopment from "../assets/webDevelopment.png";
 
 const Services = () => {
   const services = [
     { id: 1, name: "Graphic Design", img: graphicDesign },
     { id: 2, name: "Web Development", img: webDevelopment },
-    { id: 3, name: "App Development", img: appDevelopment },
+    { id: 3, name: "Game Development", img: gameDevelopment },
   ];
   return (
     <div className="bg-primary h-screen w-full flex justify-center items-center">
@@ -15,11 +15,20 @@ const Services = () => {
         <h1 className="mt-6 text-xl font-bold text-secondary uppercase underline underline-offset-8 py-6 text-center md:text-2xl">
           What I do
         </h1>
-        <div className="grid grid-row-3 gap-4">
+        <div className="grid grid-row-3 gap-4 md:flex justify-evenly">
           {services.map(({ id, name, img }) => (
-            <div key={id} className="border-8 rounded-lg p-4 text-secondary">
-              <img src={img} alt="services" className="h-16 px-16" />
-              <div className="text-center font-medium pt-2">{name}</div>
+            <div
+              key={id}
+              className="border-8 rounded-lg p-4 text-secondary md:m-4"
+            >
+              <img
+                src={img}
+                alt="services"
+                className="h-16 px-16 sm:20 md:h-40"
+              />
+              <div className="text-center font-medium pt-2 md:font-bold text-xl">
+                {name}
+              </div>
             </div>
           ))}
         </div>
