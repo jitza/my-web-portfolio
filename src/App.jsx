e9db2c206bf6f6cafb660fa7af5e0558d9b95ad1
jitza <jitza@shc.edu.bz>
2023-02-01 09:13:15 -0600

    Removed routes element and added reactFullPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
+import ReactFullpage from "@fullpage/react-fullpage";
+import "./index.css";
 
-const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-    </>
-  );
-};
+const anchors = ["firstPage", "secondPage", "thirdPage"];
+
+const App = () => (
+  <ReactFullpage
+    licenseKey={"gplv3-license"}
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#7fff00", "#00ffff", "#29ab87"]}
+    render={({ state, fullpageApi }) => {
+      return (
+        <div>
+          <div className="section">
+            <h3>Section 1</h3>
+          </div>
+          <div className="section">
+            <h3>Section 2</h3>
+          </div>
+          <div className="section">
+            <h3>Section 3</h3>
+          </div>
+        </div>
+      );
+    }}
+  />
+);
 
 export default App;
