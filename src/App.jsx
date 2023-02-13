24c91643302c4188c1b8d3e50d1f9c4b9a735a91
jitza <jitza@shc.edu.bz>
2023-02-13 16:41:18 -0600

    Added a node along with the nav bar and the reaact full page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,41 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
-import Projects from "./components/Projects";
+import Portfolio from "./components/Portfolio";
 import ReactFullpage from "@fullpage/react-fullpage";
 import "./index.css";
 
-const anchors = ["home", "about", "services", "projects"];
+const anchors = ["home", "about", "services", "portfolio"];
 
 const App = () => (
-  <ReactFullpage
-    licenseKey={"gplv3-license"}
-    anchors={anchors}
-    navigation
-    navigationTooltips={anchors}
-    sectionsColor={["#7fff00", "#00ffff", "#29ab87"]}
-    render={({ state, fullpageApi }) => {
-      return (
-        <div>
-          <NavBar />
-          <div className="section">
-            <Home />
+  <>
+    <NavBar />
+    <ReactFullpage
+      licenseKey={"gplv3-license"}
+      anchors={anchors}
+      navigation
+      navigationTooltips={anchors}
+      // sectionsColor={["#7fff00", "#00ffff", "#29ab87"]}
+      render={({ state, fullpageApi }) => {
+        return (
+          <div>
+            <div className="section">
+              <Home />
+            </div>
+            <div className="section">
+              <About />
+            </div>
+            <div className="section">
+              <Services />
+            </div>
+            <div className="section">
+              <Portfolio />
+            </div>
           </div>
-          <div className="section">
-            <About />
-          </div>
-          <div className="section">
-            <Services />
-          </div>
-          <div className="section">
-            <Projects />
-          </div>
-        </div>
-      );
-    }}
-  />
+        );
+      }}
+    />
+  </>
 );
 
 export default App;
