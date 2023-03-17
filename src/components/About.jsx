5ff4766babdcb0b9c22e877f55913fc78a558308
jitza <jitza@shc.edu.bz>
2023-03-16 22:09:35 -0600

    Updated the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,12 +12,12 @@ const About = () => {
           developer; I love to solve design problems, implement innovative web
           features and develop applications with various development tools and
           techniques. Although my work is mainly focused on front-end
-          development, as a graphic designer, I’m involved in advertising and
-          marketing designs. In addition, game development is a self-taught
-          skill I’ve acquired by programming in my free time. I have programmed
-          a couple of games that simulate traditional games, and I’ve also
-          developed other working prototypes. I am committed to developing my
-          skills and exploring every facet of the IT industry.
+          development, as a graphic designer, I’m involved in creating marketing
+          designs. In addition, game development is a self-taught skill I’ve
+          acquired by programming in my free time. I have programmed a couple of
+          games that simulate traditional games, and I’ve also developed other
+          working prototypes. I am committed to developing my skills and
+          exploring every facet of the IT industry.
         </p>
       </section>
     </div>
