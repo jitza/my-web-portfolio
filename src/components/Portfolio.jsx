f27df3b2e040ce8b9ca46cd1552b8ccd9af9d861
jitza <jitza@shc.edu.bz>
2023-05-07 10:35:49 -0600

    Added projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,11 +3,46 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import test2 from "../assets/test2.png";
-import test from "../assets/test.png";
+import hangman_game from "../assets/projects/hangman_game.png";
+import shooting_game from "../assets/projects/shooting_game.png";
+import cafeteria from "../assets/projects/cafeteria.png";
+import photobook from "../assets/projects/photobook.png";
+import charts from "../assets/projects/charts.png";
 import { EffectCoverflow, Pagination, Keyboard, Autoplay } from "swiper";
 
 const Portfolio = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "Hangman Game",
+      img: hangman_game,
+      link: "https://github.com/jitza/Hangman.git",
+    },
+    {
+      id: 2,
+      name: "Shooting Game",
+      img: shooting_game,
+      link: "https://github.com/jitza/ShootingGameVol1.git",
+    },
+    {
+      id: 3,
+      name: "Cafeteria Simulator",
+      img: cafeteria,
+      link: "https://github.com/jitza/CafeteriaSimulator.git",
+    },
+    {
+      id: 4,
+      name: "Photo Book",
+      img: photobook,
+      link: "https://github.com/jitza/CafeteriaSimulator.git",
+    },
+    {
+      id: 5,
+      name: "Charts",
+      img: charts,
+      link: "https://github.com/jitza/Charts.git",
+    },
+  ];
   return (
     <div className="h-screen w-full flex justify-center items-center bg-gradient-to-b from-slate-900 to-gray-900 text-gray-500">
       <Swiper
@@ -31,33 +66,13 @@ const Portfolio = () => {
         pagination={{ clickable: true }}
         modules={[EffectCoverflow, Pagination, Keyboard, Autoplay]}
       >
-        <SwiperSlide>
-          <img src={test2} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={test} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
+        {projects.map(({ id, name, img, link }) => (
+          <SwiperSlide>
+            <a href={link}>
+              <img src={img} />
+            </a>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
