c968260b71c31b1153aeb90a45e4f67ed66ef8f2
jitza <jitza@shc.edu.bz>
2023-05-22 18:43:05 -0600

    Fixed home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,44 +1,33 @@
 import React from "react";
 import SocialLinks from "./SocialLinks";
-import myProfile from "../assets/myProfile.jpeg";
-import myProfileimg from "../assets/myProfile.png";
+import myProfile from "../assets/myProfile.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full flex justify-center items-center ">
-      <div className="flex flex-col gap-8 md:grid grid-cols-2 grid-flow-row">
-        <div className="flex justify-center md:hidden">
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex flex-col md:grid grid-cols-2 grid-flow-row">
+        <section>
           <img
             src={myProfile}
             alt="myProfile"
-            className="rounded-lg"
-            width="280"
+            className="h-96 p-6 md:absolute md:-translate-y-24"
           />
-        </div>
-        <div className="hidden md:flex justify-center">
-          <img
-            src={myProfileimg}
-            alt="myProfile"
-            className="h-96 absolute -translate-y-24"
-          />
-        </div>
-        <div className="grid grid-row-4 gap-3">
+        </section>
+        <section className="grid grid-row-4 gap-3">
           <h1 className="text-6xl font-bold md:text-7xl">Jenny Itza</h1>
           <h2 className="text-xl font-bold uppercase">Front End Developer</h2>
           <SocialLinks />
-          <div className="hidden md:flex mt-2">
-            <a
-              href="#portfolio"
-              className="cursor-pointer group text-white w-fit px-6 py-2 flex items-center rounded-md bg-rose-500 hover:bg-rose-700"
-            >
-              Portfolio
-              <span>
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-              </span>
-            </a>
-          </div>
-        </div>
+          <a
+            href="#portfolio"
+            className="hidden md:flex items-center w-fit px-6 py-2 mt-2 rounded-md cursor-pointer group text-white bg-rose-500 hover:bg-rose-700"
+          >
+            Portfolio
+            <span>
+              <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+            </span>
+          </a>
+        </section>
       </div>
     </div>
   );
