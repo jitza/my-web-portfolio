b817f57d9543517ec19037fdfa9d3e522660501b
jitza <jitza@shc.edu.bz>
2023-09-29 14:25:40 -0600

    Added array of experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,73 +1,83 @@
 import React from "react";
 import { MdDoubleArrow } from "react-icons/md";
-import test5 from "../assets/bg/test5.png";
+import test6 from "../assets/bg/test6.png";
 
 const Experience = () => {
+  const experiences = [
+    {
+      id: 1,
+      name: "html",
+    },
+    {
+      id: 2,
+      name: "js",
+    },
+    {
+      id: 3,
+      name: "css",
+    },
+    {
+      id: 4,
+      name: "github",
+    },
+    {
+      id: 5,
+      name: "react.js",
+    },
+    {
+      id: 6,
+      name: "vue.js",
+    },
+    {
+      id: 7,
+      name: "tailwind",
+    },
+    {
+      id: 8,
+      name: "graph ql",
+    },
+    {
+      id: 9,
+      name: "postgresql",
+    },
+    {
+      id: 10,
+      name: "c++",
+    },
+    {
+      id: 11,
+      name: "node",
+    },
+    {
+      id: 12,
+      name: "sql",
+    },
+    {
+      id: 13,
+      name: "pinia",
+    },
+    {
+      id: 14,
+      name: "mui",
+    },
+  ];
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <section>
         <h1 className="text-xl font-bold uppercase underline underline-offset-8 text-center md:text-2xl">
           Tech I've Work With
         </h1>
-        <img src={test5} alt="experiences" className="absolute-center" />
-        {/* <ul className="uppercase font-bold">
-          <li>
-            <MdDoubleArrow />
-            html
-          </li>
-          <li>
-            <MdDoubleArrow />
-            js
-          </li>
-          <li>
-            <MdDoubleArrow />
-            c++
-          </li>
-          <li>
-            <MdDoubleArrow />
-            sql
-          </li>
-          <li>
-            <MdDoubleArrow />
-            css
-          </li>
-          <li>
-            <MdDoubleArrow />
-            github
-          </li>
-          <li>
-            <MdDoubleArrow />
-            react js
-          </li>
-          <li>
-            <MdDoubleArrow />
-            vue js
-          </li>
-          <li>
-            <MdDoubleArrow />
-            tailwind css
-          </li>
-          <li>
-            <MdDoubleArrow />
-            graph ql
-          </li>
-          <li>
-            <MdDoubleArrow />
-            postgres
-          </li>
-          <li>
-            <MdDoubleArrow />
-            docker
-          </li>
-          <li>
-            <MdDoubleArrow />
-            mui
-          </li>
-          <li>
-            <MdDoubleArrow />
-            sass
-          </li>
-        </ul> */}
+        <div className=" flex justify-center items-center">
+          <img src={test6} alt="experiences" />
+          <ul className="uppercase font-bold text-xl absolute left-96">
+            {experiences.map(({ id, name }) => (
+              <li key={id} className="flex items-center">
+                <MdDoubleArrow />
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </div>
   );
