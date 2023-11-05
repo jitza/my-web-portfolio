25bf81e5b31c9a02ccc23407941a7a2e6f0c39a6
jitza <jitza@shc.edu.bz>
2023-11-05 16:22:22 -0600

    Completed form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,17 +8,23 @@ const Contact = () => {
       <h1 className="absolute top-32 text-xl font-bold uppercase underline underline-offset-8 text-center md:text-2xl">
         How to get In Touch With Me!
       </h1>
-      <form className="flex flex-col p-6 px-10 w-full md:w-1/2 md:border-2">
+      <form
+        className="flex flex-col p-6 px-10 w-full md:w-1/2 md:border-2"
+        action="https://getform.io/f/868e8ac1-8001-4bf8-a83c-9fa8be7ba595"
+        method="POST"
+      >
         <input
           type="text"
           name="name"
           placeholder="Name"
+          required
           className="mt-4 p-2 bg-transparent border-b-2 text-white focus:outline-none"
         />
         <input
-          type="text"
+          type="email"
           name="email"
           placeholder="E-mail"
+          required
           className="my-4 p-2 bg-transparent border-b-2 text-white focus:outline-none"
         />
         <textarea
