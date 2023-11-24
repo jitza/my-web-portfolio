9d95c701f4f2d87ae11f53b47096f050e168dcd7
jitza <jitza@shc.edu.bz>
2023-11-24 11:05:35 -0600

    Created a slide transition of the profile pic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SocialLinks from "./SocialLinks";
 import myProfile from "../assets/myProfile.png";
 import testProfile from "../assets/testProfile.jpg";
@@ -6,31 +6,43 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { BsChevronDoubleDown } from "react-icons/bs";
 
 const Home = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      imageRef.current.classList.remove("-translate-y-full");
+    }, 100);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex flex-col md:grid grid-cols-2 grid-flow-row">
-        <section>
+        <section className="overflow-hidden relative">
           <img
+            ref={imageRef}
             src={testProfile}
             alt="myProfile"
-            className="h-96 p-6 md:absolute md:-translate-y-24"
+            className="h-96 p-6 -translate-y-full transition-transform ease-in duration-1000"
           />
         </section>
-        <section className="grid grid-row-4 gap-3">
-          <h1 className="text-6xl font-bold md:text-7xl hover:text-rose-500">
-            Jenny Itza
-          </h1>
-          <h2 className="text-xl font-bold uppercase">Web Developer</h2>
-          <SocialLinks />
-          <a
-            href="#portfolio"
-            className="hidden md:flex items-center w-fit px-6 py-2 mt-2 rounded-md cursor-pointer group text-white bg-rose-500 hover:bg-rose-700"
-          >
-            Portfolio
-            <span>
-              <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-            </span>
-          </a>
+        <section className="flex items-center">
+          <div className="grid grid-row-4 gap-3">
+            <h1 className="text-6xl font-bold md:text-7xl hover:text-rose-500">
+              Jenny Itza
+            </h1>
+            <h2 className="text-xl font-bold uppercase">Web Developer</h2>
+            <SocialLinks />
+            <a
+              href="#portfolio"
+              className="hidden md:flex items-center w-fit px-6 py-2 mt-2 rounded-md cursor-pointer group text-white bg-rose-500 hover:bg-rose-700"
+            >
+              Portfolio
+              <span>
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </a>
+          </div>
         </section>
       </div>
       <a
