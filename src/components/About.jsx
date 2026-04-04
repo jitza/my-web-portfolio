4129de6c793c2a0e42e89de7dddfca85086e32f7
jitza <jitza@shc.edu.bz>
2026-04-04 00:20:39 -0600

    Update the about page to be expandible

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import { useState, useRef } from "react";
+import { FaChevronDown } from "react-icons/fa";
+import { FaChevronUp } from "react-icons/fa";
 
 const About = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const contentRef = useRef(null);
+
+  function handleClick() {
+    setIsExpanded(!isExpanded);
+  }
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <section className="animated-left text-center mx-4 pt-8 md:px-32">
         <h1 className="text-xl font-heading font-bold uppercase underline underline-offset-8 md:text-2xl">
           Who Am I
         </h1>
-        <div className="p-6 md:text-xl">
+        <div className="p-6 flex flex-col items-center md:text-xl">
           Hello, I am Jenny, a versatile creative professional with expertise in
-          web development, graphic design, and app development. I have a passion
-          for tackling design challenges, implementing innovative web features,
-          and creating applications using a variety of development tools and
-          techniques. While my primary focus centers on front-end development, I
-          also lend my creative talents to the realms of advertising and
-          marketing as a graphic designer. My journey has been characterized by
-          active involvement in numerous app projects, each contributing to the
-          depth and breadth of my knowledge in the field of app development. My
-          unwavering commitment to self-improvement fuels my passion for
-          continuous skill enhancement and professional advancement. I have full
-          confidence in my capacity to utilize my expertise and abilities for
-          ongoing personal and career growth.
+          web development, graphic design, and app development.{" "}
+          <button
+            className="flex flex-col items-center text-sm w-fit px-2 py-1 m-2 rounded-md cursor-pointer group text-white bg-rose-500 hover:bg-rose-700"
+            onClick={handleClick}
+          >
+            <span>{isExpanded ? "Show Less" : "Show More"}</span>
+            <span>
+              {isExpanded ? (
+                <FaChevronUp size={15} />
+              ) : (
+                <FaChevronDown size={15} />
+              )}
+            </span>
+          </button>
+          <div
+            style={{
+              maxHeight: isExpanded
+                ? `${contentRef.current?.scrollHeight}px`
+                : "0px",
+            }}
+            className="overflow-hidden transition-all duration-500 ease-in-out"
+          >
+            <div ref={contentRef}>
+              <p>
+                I have a passion for tackling design challenges, implementing
+                innovative web features, and creating applications using a
+                variety of development tools and techniques. While my primary
+                focus centers on front-end development, I also lend my creative
+                talents to the realms of advertising and marketing as a graphic
+                designer. My journey has been characterized by active
+                involvement in numerous app projects, each contributing to the
+                depth and breadth of my knowledge in the field of app
+                development. My unwavering commitment to self-improvement fuels
+                my passion for continuous skill enhancement and professional
+                advancement. I have full confidence in my capacity to utilize my
+                expertise and abilities for ongoing personal and career growth.
+              </p>
+            </div>
+          </div>
+
         </div>
       </section>
     </div>
@@ -28,3 +66,4 @@ const About = () => {
 };
 
 export default About;
+
